3b6dc7e7fc4298612f6505e6c234ae3c805cdf7e
fatema <fatema@chaldal.com>
2020-06-15 19:29:14 +0600

    git pages

---
+++ build/precache-manifest.6cb218140811ce945ce9cba89968d4fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4910e582513deead9905fb574a11e39d",
+    "url": "/coronatrackerbd/index.html"
+  },
+  {
+    "revision": "2cf6f70b5fe58e5c15ef",
+    "url": "/coronatrackerbd/static/css/main.a1d846d9.chunk.css"
+  },
+  {
+    "revision": "e52783c2666f1dfe52f3",
+    "url": "/coronatrackerbd/static/js/2.4c575c9d.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/coronatrackerbd/static/js/2.4c575c9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cf6f70b5fe58e5c15ef",
+    "url": "/coronatrackerbd/static/js/main.4e9d6d26.chunk.js"
+  },
+  {
+    "revision": "de24c5db609e2b912146",
+    "url": "/coronatrackerbd/static/js/runtime-main.d5a59f35.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/coronatrackerbd/static/media/image.d7265326.png"
+  }
+]);
